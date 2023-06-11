58fdf5df2d71142b5d8f5d69c6d98bf0be7c237a
LyricsON <lyricson0203@gmail.com>
2023-06-11 16:31:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { moviesData } from "./data";
+import MovieList from "./Components/MovieList";
+import AddNewMovie from "./Components/AddNewMovie";
+import Surch from "./Components/Surch";
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+  const [filterText, setFilterText] = useState("gu");
+  const [filterRating, setFilterRating] = useState(1);
+  const handleTextFiler = (x) => setFilterText(x);
+  const handleRatingFilter = (y) => setFilterRating(y);
+  // console.log(movies);
+  // deletefunction
+  const handleDelete = (theId) =>
+    setMovies(movies.filter((el) => el.id !== theId));
+  // add new movie
+  const handleAdd = (newMovie) => setMovies([...movies, newMovie]);
+  // edit movie
+  const handleEdit = (editedMovie) =>
+    setMovies(
+      movies.map((el) =>
+        el.id === editedMovie.id ? { ...el, ...editedMovie } : el
+      )
+    );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Surch
+        filterText={filterText}
+        filterRating={filterRating}
+        handleTextFiler={handleTextFiler}
+        handleRatingFilter={handleRatingFilter}
+      />
+      <MovieList
+        list={movies.filter(
+          (el) =>
+            el.name.toLowerCase().includes(filterText.toLowerCase()) &&
+            el.rating >= filterRating
+        )}
+        deleteFunction={handleDelete}
+        handleEdit={handleEdit}
+      />
+      <AddNewMovie handleAdd={handleAdd} />
     </div>
   );
 }
